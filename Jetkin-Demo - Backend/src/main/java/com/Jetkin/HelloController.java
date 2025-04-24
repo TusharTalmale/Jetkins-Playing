a73d623ebcf1f898569ad4d1e2927431bbbd0597
Tushar Talmale <tushartal2@gmail.com>
2025-04-24 23:17:44 +0530

    added

--- Jetkin-Demo - Backend/src/main/java/com/Jetkin/HelloController.java
+++ Jetkin-Demo - Backend/src/main/java/com/Jetkin/HelloController.java
@@ -1,11 +1,13 @@
 package com.Jetkin;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class HelloController {
     @GetMapping("/api/time")
